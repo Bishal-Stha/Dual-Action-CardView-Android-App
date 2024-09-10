962fdc123e604b90df097024aaac085e77a01091
Bishal Shrestha <programmerbishal@gmail.com>
2024-09-10 08:11:52 +0545

    Pushed ...

---
+++ app/src/main/java/com/example/implicitactivity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.implicitactivity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val website = findViewById<androidx.cardview.widget.CardView>(R.id.website)
+        val camera = findViewById<androidx.cardview.widget.CardView>(R.id.camera)
+        website.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://bishalstha.netlify.app/"))
+            startActivity(intent)
+        }
+        camera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+    }
+}
